562c247748d1608489a3d9092f321552579261df
Saman Bemel-Benrud <samanpwbb@gmail.com>
2012-05-14 12:55:53 -0400

    added play button toggle and wrote description.

--- tweetRace.js
+++ tweetRace.js
@@ -166,12 +166,13 @@ tweetRace.map = function() {
 // Pause / play button
 $('#stop').click(function(e){
     e.preventDefault();
+    $(this).toggleClass('start');
     if (tweetRace.stop) {
         $(this).text('Pause');
         tweetRace.stop = false;
         tweetRace.start();
     } else { 
-        $(this).text('Play');
+        $(this).text('Start');
         tweetRace.stop = true;
     }
 });
